7e2308639ee19b2486d618ef9e4d2ed95aab73a0
Florencekyarikunda <florencekyarikund@gmail.com>
2021-03-04 19:54:03 +0300

    first commit

---
+++ src/prosper.kt
@@ -0,0 +1,28 @@
+fun main() {
+  addition ()
+    println("mummy")
+    addition()
+    println()
+    modulus()
+
+}
+fun addition(){
+    var p: Int=30
+    var q: Int=27
+    var total=p + q
+    println(total)
+
+}
+fun mummy() {
+    println("mummy")
+}
+fun add(p: Int,q: Int):Int{
+ var total=return p+q
+    return total
+}
+fun modulus(){
+    var d=46
+    var m=12
+    var modulus="46%12"
+    println(modulus)
+}
